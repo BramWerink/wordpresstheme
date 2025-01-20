3685e9c095e2a4ced0c8dbd66a21cdf13551f244
Bram Werink <bramwerink@gmail.com>
2025-01-20 11:50:37 +0100

    wip block

--- bramwerink/blocks/textandimage/block.php
+++ bramwerink/blocks/textandimage/block.php
@@ -1,3 +1,18 @@
 <div class="textandimage">
-    <p>Text7image</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="content">
+                    <p>Subtext</p>
+                    <h2>Title</h2>
+                    <p>Text field that contains some text</p>
+                    <a href="" class="button-primary">Button</a>
+                </div>
+            </div>
+            <div class="col-md-3">a</div>
+            <div class="col-md-3">b</div>
+            <div class="col-md-3">c</div>
+            <div class="col-md-3">d</div>
+        </div>
+    </div>
 </div>
