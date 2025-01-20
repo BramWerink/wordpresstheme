bd3742d45ff90a68d55b3e7de60945f0f8f862d2
Bram Werink <bramwerink@gmail.com>
2025-01-20 14:00:32 +0100

    added layout and settings support for page editing

--- bramwerink/blocks/textandimage/block.php
+++ bramwerink/blocks/textandimage/block.php
@@ -9,10 +9,15 @@
                     <a href="" class="button-primary">Button</a>
                 </div>
             </div>
-            <div class="col-md-3">a</div>
-            <div class="col-md-3">b</div>
-            <div class="col-md-3">c</div>
-            <div class="col-md-3">d</div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-3">a</div>
+                    <div class="col-md-3">b</div>
+                    <div class="col-md-3">c</div>
+                    <div class="col-md-3">d</div>
+                </div>
+            </div>
+            
         </div>
     </div>
 </div>
